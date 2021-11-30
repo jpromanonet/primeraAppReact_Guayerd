640835fbeeaca3d44dfaf31b12f715eae010dbfc
jpromanonet <programming@jpromano.net>
2021-11-30 12:41:41 -0300

    Body component

---
+++ portfolio/src/Components/body/about/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import SocialContact from "../../common/social-contact";
+import "./about.css";
+
+function About() {
+    return(
+        <div className="about">
+            <div className="about-top">
+                <div className="about-info">
+                    Hola, Soy Alexis 👋 y bienvenide!
+                    <br />
+                    <br />
+                    Soy desarrollador front-end.
+                </div>
+                <div className="about-photo">
+                    <img 
+                        src={requestAnimationFrame("../../../assets/image/coding.png").default}
+                        className="picture"
+                    />
+                </div>
+                <SocialContact />
+            </div>
+        </div>
+    );
+}
