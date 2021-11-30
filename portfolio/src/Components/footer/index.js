f60a678fa87f3f7f420fce4706f5ed0787417e5b
jpromanonet <programming@jpromano.net>
2021-11-30 12:18:02 -0300

    Body component

---
+++ portfolio/src/Components/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+    return <div className="footer">Hecho con ❤️</div>;
+}
+
+export default Footer;
--- portfolio/src/Components/home/index.js
+++ portfolio/src/Components/home/index.js
@@ -1,12 +1,20 @@
 import React, { cloneElement } from "react";
 import "./home.css";
-import Header from "../header/index"
+import Header from "../header/index";
+import Body from "../body/index";
+import Footer from "../footer/index";
 
 function Home(){
     return (
         <div className="home">
             <div>
                 <Header />
+            </div>
+            <div>
+                <Body />
+            </div>
+            <div>
+                <Footer />
             </div>
         </div>
     );
