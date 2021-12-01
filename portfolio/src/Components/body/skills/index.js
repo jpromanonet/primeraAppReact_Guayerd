946fa5b0c3a008050ad5ab56030f59c7942ae3f7
jpromanonet <programming@jpromano.net>
2021-12-01 11:10:09 -0300

    Contact component

---
+++ portfolio/src/Components/body/skills/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./skills.css";
+import { SkillsData } from "../../../data/skils"
+import SkillCard from "./skill-card";
+import Separator from "../../common/separator";
+
+function Skills() {
+    const data = SkillsData;
+    return(
+        <div className="skills">
+            <Separator />
+            <label className="section-data">Skills</label>
+            <div className="skills-container">
+                {data.map((item) => {
+                    return(
+                        <div className="skills-section">
+                            <label className="skills-section-title">{item.type}</label>
+                            <div className="skills-list">
+                                {item.list.map((skill) => {
+                                    return <SkillCard skill={skill} />;
+                                })}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
